aeec69db94a5bf3ff31074fc2537fb51b2f953bc
Reegan <reegan.gunasekaran@neosoftmail.com>
2024-04-29 11:00:08 +0530

    second commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import SignInSide from "@/Components/Login/Signin";
+import Signin from "@/Components/Login/Signin";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+    <div>
+      <SignInSide />
+     
+    </div>
+  );
+}
